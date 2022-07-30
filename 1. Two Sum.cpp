f57a02a6e07554b79f6817eca6f251a29d1fddb1
215355 <84146574+Zisess@users.noreply.github.com>
2022-07-30 15:40:52 +0300

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -12,4 +12,20 @@ public:
     }
     return sacmalik;
     }
+    
+    vector<int> twoSum_v2(vector<int>& nums, int target) {
+    map<int, int> hashmap;
+    vector <int> retn;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (hashmap.find(target - nums[i]) != hashmap.end())
+        {
+            retn.push_back(hashmap[target - nums[i]]);
+            retn.push_back(i);
+            break;
+        }
+        hashmap[nums[i]] = i;
+    }
+    return retn;
+}
 };
